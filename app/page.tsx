bbf61f78d864a7d14d2e31e92440846390731092
GarimaUttam <garimauttam191203@gmail.com>
2025-02-28 11:17:22 +0530

    navbar and mobileNav

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Home from '@/components/Home/Home';
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='text-5xl'>HomePage</div>
+    <div className='overflow-hidden'>
+      <Home />
+    </div>
   )
 }
 
