4000330484e91f1ed208418f3d5043b6af35b989
GarimaUttam <garimauttam191203@gmail.com>
2025-03-03 02:05:14 +0530

    Contact Section done

--- components/Home/Contact/Contact.tsx
+++ components/Home/Contact/Contact.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
         <ContactForm />
         <div>
           {/* Contact info */}
-          <div>
+          <div className="xl:mx-auto">
             <ContactInfo />
           </div>
         </div>
