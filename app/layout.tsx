bde52349dc7c049cf3f189c1b975c6a610f09f8d
GarimaUttam <garimauttam191203@gmail.com>
2025-03-03 10:08:07 +0530

    scrollbar and cursor

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,11 @@ import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
 import ScrollToTop from "@/components/Helper/ScrollToTop";
+import AnimatedCursor from "react-animated-cursor";
 
 const font = Sora({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
-  subsets: ['latin']
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -21,9 +22,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={font.className}>
+    <html lang="en" className="custom-scrollbar">
+      <body className={font.className}>
+        <div className="hidden md:block">
+          <AnimatedCursor
+            innerSize={8}
+            outerSize={35}
+            innerScale={2}
+            outerScale={2}
+            outerAlpha={0}
+            innerStyle={{
+              backgroundColor: "white",
+            }}
+            outerStyle={{
+              border: "3px solid white",
+             }}
+          />
+        </div>
         <ResponsiveNav />
         {children}
         <Footer />
