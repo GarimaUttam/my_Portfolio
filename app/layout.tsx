dd24717401078ec82aac143b651f333be52a9845
GarimaUttam <garimauttam191203@gmail.com>
2025-03-03 09:10:24 +0530

    scrollToTop done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Sora } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 const font = Sora({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
@@ -25,7 +26,8 @@ export default function RootLayout({
         className={font.className}>
         <ResponsiveNav />
         {children}
-        <Footer/>
+        <Footer />
+        <ScrollToTop />
       </body>
     </html>
   );
